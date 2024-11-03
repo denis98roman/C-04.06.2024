11a8699192bb0395e6f3883f4442ef1ce2aa5766
Денис <lotos.d21@gmail.com>
2024-11-03 20:14:31 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine(Sum(1));
+int Sum(int x)
+{
+    if (x >= 0)
+    {
+        Console.WriteLine("false");
+    }
+    else
+    {
+        Console.WriteLine("true");
+    }
+    return x;
+}
+
+
+
+
